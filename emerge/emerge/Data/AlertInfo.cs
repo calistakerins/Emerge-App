979905504fd7ab738756c7430a4ff4dddfa7c9a3
megantrieu <meganatrieu@gmail.com>
2022-10-30 23:53:47 +0100

    view Alert Page

---
+++ emerge/emerge/Data/AlertInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace emerge.Data
+{
+    public class AlertInfo
+    {
+        private DateTime dateTime;
+        private String title;
+        private String author;
+        private UpdateInfo[] updates;
+
+        public AlertInfo(DateTime dateTime, string title, string author, UpdateInfo[] updates)
+        {
+            this.dateTime = dateTime;
+            this.Title = title;
+            this.Author = author;
+            this.Updates = updates;
+        }
+
+        public DateTime DateTime { get => dateTime; set => dateTime = value; }
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public UpdateInfo[] Updates { get => updates; set => updates = value; }
+    }
+}
+
---
+++ emerge/emerge/Data/UpdateInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace emerge.Data
+{
+    public class UpdateInfo
+    {
+        private DateTime dateTime;
+        private String description;
+
+        public UpdateInfo(DateTime dateTime, string description)
+        {
+            this.dateTime = dateTime;
+            this.Description = description;
+        }
+
+        public DateTime DateTime { get => dateTime; set => dateTime = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
+
