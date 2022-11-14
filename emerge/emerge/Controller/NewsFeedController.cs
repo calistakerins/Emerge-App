2f741c82a7f10260fac44c26b63d6a79edef42b4
Harrison Shu <hshu02@calpoly.edu>
2022-11-14 19:17:46 +0100

    looping through alerts programatically w/ dummy data

--- emerge/emerge/Controller/NewsFeedController.cs
+++ emerge/emerge/Controller/NewsFeedController.cs
@@ -1,11 +1,40 @@
 ﻿using System;
+using emerge.Data;
+using static MudBlazor.FilterOperator;
+
 namespace emerge.Controller
 {
-	public class NewsFeedController
+    
+    public class NewsFeedController
 	{
-		public NewsFeedController()
+		private List<AlertInfo> alertList;
+
+        //dummy data
+        private static readonly string title = "FLASH FLOOD EVACUATION";
+        private static readonly string author = "TOWN ADMIN";
+        private static readonly System.DateTime dateTime = new System.DateTime(2019, 05, 09, 09, 15, 00);
+        private static readonly UpdateInfo[] updates = {
+        new UpdateInfo(new System.DateTime(2019, 05, 09, 09, 37, 07), "Integer risus nunc, mattis in ornare sit amet, aliquam quis ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut mauris massa, iaculis non augue vitae, mattis tincidunt turpis. In laoreet turpis leo, ut laoreet leo blandit feugiat."),
+        new UpdateInfo(new System.DateTime(2019, 05, 09, 10, 56, 41), "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut mauris massa, iaculis non augue vitae, mattis tincidunt turpis. In laoreet turpis leo, ut laoreet leo blandit feugiat.")};
+
+        //populates alertList
+        public NewsFeedController()
 		{
-		}
+            alertList = new List<AlertInfo>();
+            AlertInfo dummyAlert = new AlertInfo(dateTime, title, author, updates);
+            this.addAlert(dummyAlert);
+        }
+
+        public List<AlertInfo> getAlertList()
+        {
+            return alertList;
+        }
+
+        public void addAlert(AlertInfo alertInfo)
+        {
+            alertList.Add(alertInfo);
+        }
+
 	}
 }
 
