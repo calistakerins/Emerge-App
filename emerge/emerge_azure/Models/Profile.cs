bd4b9fae983d1d710cc5250bec4b95c49e673c74
calistakerins <71235670+calistakerins@users.noreply.github.com>
2022-11-20 15:06:50 +0100

    Add files via upload

---
+++ emerge/emerge_azure/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserDatabase.Models
+{
+	public class Profile
+	{
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+        [JsonProperty(PropertyName = "admin")]
+        public bool Admin { get; set; }
+	}
+}
+
---
+++ emerge/emerge_azure/Models/ProfileStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserDatabase.Models
+{
+	public class ProfileStore
+	{
+        public static List<Profile> users = new List<Profile>();
+    }
+}
+
