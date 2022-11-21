2090079e9cfcc8bc8de44ffec80c1bd662ffceb3
Harrison Shu <hshu02@calpoly.edu>
2022-11-21 14:53:44 +0100

    got bson doc data from db

--- emerge/emerge_azure/AlertFuncs.cs
+++ emerge/emerge_azure/AlertFuncs.cs
@@ -8,15 +8,28 @@ using System.Threading.Tasks;
 using emerge_azure.Models;
 using Newtonsoft.Json;
 using System.IO;
+using MongoDB.Driver;
+using System;
+using MongoDB.Bson;
 
 public static class AlertFuncs
 {
+    public static readonly string connectionString = "mongodb+srv://emerge:project3@cluster0.ztzvtkd.mongodb.net/?retryWrites=true&w=majority";
+
     [FunctionName("get_newsalert")]
     public static async Task<IActionResult> GetNewsAlerts(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "newsalert")] HttpRequest req,
         ILogger log)
     {
         log.LogInformation("Called get_newsalert with GET request");
+        MongoClient dbClient = new MongoClient(connectionString);
+        var database = dbClient.GetDatabase("alerts");
+        var collection = database.GetCollection<BsonDocument>("alertInfo");
+        var documents = collection.AsQueryable();
+        foreach (BsonDocument alert in documents)
+        {
+            Console.Write(alert["Title"].AsString);
+        }
 
         return new OkObjectResult(NewsAlertFeed.newsalerts);
     }
