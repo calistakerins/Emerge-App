71ff5f559f1a8424b52ae6865fe3bc62d9795344
calistakerins <71235670+calistakerins@users.noreply.github.com>
2022-11-21 11:22:56 +0100

    Add files via upload

---
+++ emerge/emerge/Data/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace emerge.Data
+{
+	public class UserProfile
+	{
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+        [JsonProperty(PropertyName = "admin")]
+        public bool Admin { get; set; }
+    }
+}
+
