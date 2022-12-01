da6f4376525348070336cdf82834bed652240256
Harrison Shu <hshu02@calpoly.edu>
2022-12-01 23:33:52 +0100

    added controller edit

--- emerge/emerge/Controller/DepartmentController.cs
+++ emerge/emerge/Controller/DepartmentController.cs
@@ -21,7 +21,7 @@ namespace emerge.Controller
 
         private async void populateDepts()
         {
-            string url = "https://emergeproj.azurewebsites.net/api/depts?";
+            string url = "https://emerge-azure.azurewebsites.net/api/depts?";
             using var client = new HttpClient();
             client.BaseAddress = new Uri(url);
             // Add an Accept header for JSON format.
