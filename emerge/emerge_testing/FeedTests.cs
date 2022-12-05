8a55e8ef9981dabe5720f71662a6c02b5b016b22
Harrison Shu <hshu02@calpoly.edu>
2022-12-05 13:48:51 +0100

    adding testing project to emerge solution

---
+++ emerge/emerge_testing/FeedTests.cs
@@ -0,0 +1,15 @@
+﻿namespace emerge_testing;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
